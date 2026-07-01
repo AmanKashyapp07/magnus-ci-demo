c85269b9f56a3a252565b911c6030157ebb17b16
AMAN KASHYAP <iit2024140@iiita.ac.in>
2026-07-01 12:46:15 +0530

    Initial commit

---
+++ math.test.js
@@ -0,0 +1,11 @@
+// This is a simple test file to demonstrate MagnusCI.
+// In the interview, to demonstrate a SUCCESSFUL build, leave this as is.
+// To demonstrate the AUTO-REVERT feature, change the 2 to a 3 and push!
+
+test('basic addition logic', () => {
+  expect(1 + 1).toBe(2);
+});
+
+test('basic subtraction logic', () => {
+  expect(5 - 2).toBe(3);
+});
